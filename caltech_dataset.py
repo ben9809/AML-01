ad00b28975109c919b5e8118988604dc9bed4bb3
Benedetto <s247082@studenti.polito.it>
2022-10-27 16:52:10 +0200

    fix open files

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -11,8 +11,8 @@ import sys
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
-        img = Image.open(f)
-        return img.convert('RGB')
+        with Image.open(f) as img:
+            return img.convert('RGB')
 
 
 class Caltech(VisionDataset):
